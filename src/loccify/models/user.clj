2eb17328784720916040dda89414bef4d98da6d9
Jani Arvonen <jani.arvonen@gmail.com>
2012-10-06 20:03:11 +0300

    should find user by id from repository

--- src/loccify/models/user.clj
+++ src/loccify/models/user.clj
@@ -1,7 +1,15 @@
 (ns loccify.models.user
-	(:use [loccify.db]))
+	(:use [loccify.db])
+	(:import [org.bson.types ObjectId]))
 
+(declare query-details)
 (def user-collection-name "users")
 
 (defn create-user [user]
 	(db-insert user-collection-name user))
+
+(defn find-user-by-id [id]
+	(db-find (query-details :find-one {:_id (ObjectId. id)} user-collection-name)))
+
+(defn- query-details [type query collection-name]
+	{:find-type type :query query :collection collection-name})
