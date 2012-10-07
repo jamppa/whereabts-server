3dded1fe81d27de26d9d21198f304086f4f1b88d
Jani Arvonen <jani.arvonen@gmail.com>
2012-10-07 21:52:00 +0300

    added initial version of validation for loccage-objects when saved to db

--- src/loccify/models/loccage.clj
+++ src/loccify/models/loccage.clj
@@ -1,8 +1,15 @@
 (ns loccify.models.loccage
-	(:use [loccify.db])
+	(:use [loccify.db]
+		  [validateur.validation])
 	(:import [org.bson.types ObjectId]))
+
+(defn- create-validation-set-for-loccage []
+	(validation-set
+		(presence-of :message)
+		(presence-of :user_id)))
 
 (def loccage-collection-name "loccages")
 
 (defn save-loccage [loccage]
-	(db-insert loccage-collection-name loccage))
+	(when (valid? (create-validation-set-for-loccage) loccage)
+		(db-insert loccage-collection-name loccage)))
