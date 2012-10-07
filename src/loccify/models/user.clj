123f2d013139ce6150dfe91c9e129f41f2841d4f
Jani Arvonen <jani.arvonen@gmail.com>
2012-10-07 14:08:57 +0300

    added simple validation for user-object before saving to db

--- src/loccify/models/user.clj
+++ src/loccify/models/user.clj
@@ -1,15 +1,26 @@
 (ns loccify.models.user
-	(:use [loccify.db])
+	(:use [loccify.db]
+			[validateur.validation])
 	(:import [org.bson.types ObjectId]))
 
-(declare query-details)
+(declare create-query-details)
+(declare create-validation-set-for-user)
+ 
 (def user-collection-name "users")
+(def user-validation-set (create-validation-set-for-user))
 
-(defn create-user [user]
-	(db-insert user-collection-name user))
+(defn save-user [user]
+	(when (valid? user-validation-set user) 
+		(db-insert user-collection-name user)))
 
 (defn find-user-by-id [id]
-	(db-find (query-details :find-one {:_id (ObjectId. id)} user-collection-name)))
+	(db-find (create-query-details :find-one {:_id (ObjectId. id)} user-collection-name)))
 
-(defn- query-details [type query collection-name]
+(defn- create-query-details [type query collection-name]
 	{:find-type type :query query :collection collection-name})
+
+(defn- create-validation-set-for-user [] 
+	(validation-set 
+		(presence-of :name)
+		(presence-of :email)
+		(presence-of :_id)))
