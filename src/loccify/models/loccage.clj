48de2b6b883f2737f76d21b7b8d53c8886eae524
Jani Arvonen <jani.arvonen@gmail.com>
2012-10-07 18:29:15 +0300

    moved private functions on top of compilation unit and removed :_id from validation set for user

--- src/loccify/models/loccage.clj
+++ src/loccify/models/loccage.clj
@@ -1,1 +1,8 @@
-(ns loccify.model.loccage)
+(ns loccify.models.loccage
+	(:use [loccify.db])
+	(:import [org.bson.types ObjectId]))
+
+(def loccage-collection-name "loccages")
+
+(defn save-loccage [loccage]
+	(db-insert loccage-collection-name loccage))
--- src/loccify/models/user.clj
+++ src/loccify/models/user.clj
@@ -1,10 +1,15 @@
 (ns loccify.models.user
 	(:use [loccify.db]
-			[validateur.validation])
+		  [validateur.validation])
 	(:import [org.bson.types ObjectId]))
 
-(declare create-query-details)
-(declare create-validation-set-for-user)
+(defn- create-validation-set-for-user []
+	(validation-set 
+		(presence-of :name)
+		(presence-of :email)))
+
+(defn- create-query-details [type query collection-name]
+	{:find-type type :query query :collection collection-name})
  
 (def user-collection-name "users")
 (def user-validation-set (create-validation-set-for-user))
@@ -17,11 +22,3 @@
 	(db-find 
 		(create-query-details :find-one {:_id (ObjectId. id)} user-collection-name)))
 
-(defn- create-query-details [type query collection-name]
-	{:find-type type :query query :collection collection-name})
-
-(defn- create-validation-set-for-user [] 
-	(validation-set 
-		(presence-of :name)
-		(presence-of :email)
-		(presence-of :_id)))
