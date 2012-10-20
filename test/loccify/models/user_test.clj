53aa393c0eb0c3f19595978cdc19bd3c8db76757
Jani Arvonen <jani.arvonen@gmail.com>
2012-10-20 22:29:34 +0300

    should create correct query details map

--- test/loccify/models/user_test.clj
+++ test/loccify/models/user_test.clj
@@ -1,1 +1,11 @@
-(ns loccify.models.user-test)
+(ns loccify.models.user-test
+	(:use [midje.sweet])
+	(:use [midje.util :only [testable-privates]])
+	(:use [loccify.models.user]))
+
+(testable-privates loccify.models.user create-query-details)
+
+(def expected-query-details {:find-type :find-one :query {:key "val"} :collection "coll"})
+
+(fact "should create correct query details map"
+	(create-query-details :find-one {:key "val"} "coll") => expected-query-details)
