47ce1e4a40f13ee6c798e3ddc0ebad7f760f607b
Jani Arvonen <jani.arvonen@gmail.com>
2012-10-20 15:18:44 +0300

    should find many as maps from collection by given query-details

--- test/loccify/db_test.clj
+++ test/loccify/db_test.clj
@@ -7,6 +7,7 @@
 (def obj-to-insert {:somekey "someval"})
 (def coll-to-insert "loccify")
 (def query-find-one {:find-type :find-one :collection "coll" :query "query"})
+(def query-find-many (assoc query-find-one :find-type :find-many))
 (def query-result {:key "val"})
 
 (fact "should insert object to collection and return it"
@@ -17,3 +18,6 @@
 	(db-find query-find-one) => query-result
 	(provided (monger-collection/find-one-as-map "coll" "query") => query-result :times 1))
 
+(fact "should find many as maps from collection by given query-details"
+	(db-find query-find-many) => query-result
+	(provided (monger-collection/find-maps "coll" "query") => query-result :times 1))
