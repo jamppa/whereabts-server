5338851f02323f6293cec96d9fd6184571537d93
Jani Arvonen <jani.arvonen@gmail.com>
2012-10-20 14:52:42 +0300

    extracted own function for db-connection initialization

--- src/loccify/server.clj
+++ src/loccify/server.clj
@@ -3,11 +3,16 @@
   (:use [loccify.db]))
 
 (server/load-views-ns 'loccify.views)
-(db-connect "loccify")
+
+(def ^:dynamic *loccify-db* "loccify")
+
+(defn- init-db-connection []
+	(db-connect *loccify-db*))
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
     (server/start port {:mode mode
-                        :ns 'loccify})))
+                        :ns 'loccify})
+    (init-db-connection)))
 	
