2d7790afeebdea98ca152ae8f33529ea0945e987
Jani Arvonen <jani.arvonen@gmail.com>
2012-10-20 13:36:56 +0300

    should find one as map from collection by given query-details

--- test/loccify/db_test.clj
+++ test/loccify/db_test.clj
@@ -5,8 +5,15 @@
 			  [monger.collection :as monger-collection]))
 
 (def obj-to-insert {:somekey "someval"})
+(def coll-to-insert "loccify")
+(def query-find-one {:find-type :find-one :collection "coll" :query "query"})
+(def query-result {:key "val"})
 
 (fact "should insert object to collection and return it"
-	(db-insert "loccify-test" obj-to-insert) => obj-to-insert
-	(provided (monger-collection/insert-and-return "loccify-test" anything) => obj-to-insert :times 1 ))
+	(db-insert coll-to-insert obj-to-insert) => obj-to-insert
+	(provided (monger-collection/insert-and-return coll-to-insert anything) => obj-to-insert :times 1 ))
+
+(fact "should find one as map from collection by given query-details"
+	(db-find query-find-one) => query-result
+	(provided (monger-collection/find-one-as-map "coll" "query") => query-result :times 1))
 
