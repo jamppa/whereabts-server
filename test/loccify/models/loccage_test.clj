d8e61135a4785c7ee93cf7c730bd278ec8b9141b
Jani Arvonen <jani.arvonen@gmail.com>
2012-10-24 21:42:24 +0300

    should save valid loccage to db

--- test/loccify/models/loccage_test.clj
+++ test/loccify/models/loccage_test.clj
@@ -1,1 +1,14 @@
-(ns loccify.loccage-test)
+(ns loccify.models.loccage-test
+	(:use [midje.sweet])
+	(:use [loccify.models.loccage])
+	(:use [loccify.db])
+	(:import [org.bson.types ObjectId]))
+
+(defn create-valid-loccage []
+	{:user_id (ObjectId.) :message "dägä"})
+
+(def valid-loccage (create-valid-loccage))
+
+(fact "should save valid loccage to db"
+	(save-loccage valid-loccage) => valid-loccage
+	(provided (db-insert "loccages" valid-loccage) => valid-loccage :times 1))
