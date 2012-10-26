3e3da53fdcad8957a538940be0ad986f6c9a8f4c
Jani Arvonen <jani.arvonen@gmail.com>
2012-10-26 23:16:50 +0300

    should not save invalid loccage to db

--- test/loccify/models/loccage_test.clj
+++ test/loccify/models/loccage_test.clj
@@ -7,8 +7,16 @@
 (defn create-valid-loccage []
 	{:user_id (ObjectId.) :message "dägä"})
 
+(defn create-invalid-loccage []
+	{:message "dägä"})
+
 (def valid-loccage (create-valid-loccage))
+(def invalid-loccage (create-invalid-loccage))
 
 (fact "should save valid loccage to db"
 	(save-loccage valid-loccage) => valid-loccage
 	(provided (db-insert "loccages" valid-loccage) => valid-loccage :times 1))
+
+(fact "should not save invalid loccage to db"
+	(save-loccage invalid-loccage) => nil
+	(provided (db-insert "loccages" invalid-loccage) => nil :times 0))
