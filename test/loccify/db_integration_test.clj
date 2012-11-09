478147698874abe14e9effb15a806c2f48e4bb14
Jani Arvonen <jani.arvonen@gmail.com>
2012-11-09 22:30:08 +0200

    cleaned test a bit by adding separate function to build query-details

--- test/loccify/db_integration_test.clj
+++ test/loccify/db_integration_test.clj
@@ -1,11 +1,14 @@
 (ns loccify.db-integration-test
-	(:use [loccify.db])
+	(:use [loccify.db])	
 	(:use [midje.sweet])
 	(:use [loccify.db-helper])
 	(:import [org.bson.types ObjectId]))
+
+(defn- query-details [type coll query]
+	{:find-type type :collection coll :query query})
 
 (background (before :facts (setup-test-db)))
 
 (fact "should find one object from database by query-details"
 	(db-find 
-		{:find-type :find-one :collection "docs" :query {:_id (ObjectId. "509d513f61395f0ebbd5e32a")}}) => test-obj-a)
+		(query-details :find-one "docs" {:_id (ObjectId. "509d513f61395f0ebbd5e32a")})) => test-obj-a)
