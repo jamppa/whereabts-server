d6c7cd799edd3436e77a7ca8c3f1a6d0fbe15d13
Jani Arvonen <jani.arvonen@gmail.com>
2012-11-18 14:02:37 +0200

    should convert meters to degrees

--- src/loccify/util/geo.clj
+++ src/loccify/util/geo.clj
@@ -2,3 +2,8 @@
 
 (defn location [lon lat dist]
 	{:lon lon :lat lat :dist dist})
+
+(defn meters-to-degrees [m]
+	(-> m
+	(/ 1000)
+	(/ 111.12)))
--- test/loccify/util/geo_test.clj
+++ test/loccify/util/geo_test.clj
@@ -4,3 +4,6 @@
 
 (fact "should create location-object"
 	(location 10.0 10.0 500) => {:lon 10.0, :lat 10.0, :dist 500})
+
+(fact "should convert meters to degrees"
+	(meters-to-degrees 500) => (/ 0.5 111.12))
