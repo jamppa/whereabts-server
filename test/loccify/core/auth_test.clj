95dd5591bc36d151e0f1e6f819b050063bcd772b
Jani Arvonen <jani.arvonen@gmail.com>
2012-12-13 18:48:55 +0200

    should not be authenticated user when one is not found by email and password

--- test/loccify/core/auth_test.clj
+++ test/loccify/core/auth_test.clj
@@ -9,4 +9,8 @@
 	(authenticated? "teppo@test.fi" "secret") => true
 	(provided (find-user-by-email-and-pass "teppo@test.fi" "secret") => auth-user))
 
+(fact "should not be authenticated user when one is not found by email and password"
+	(authenticated? "teppo@test.fi" "secret") => false
+	(provided (find-user-by-email-and-pass "teppo@test.fi" "secret") => nil))
+
 
