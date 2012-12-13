d6d86aec4ca63c408a86ddf5e124100bc43a73a8
Jani Arvonen <jani.arvonen@gmail.com>
2012-12-13 20:23:37 +0200

    cleaned user querying a bit

--- src/loccify/models/user.clj
+++ src/loccify/models/user.clj
@@ -10,23 +10,23 @@
 		(presence-of :password)
 		(presence-of :type)))
 
-(defn- create-query-details [type query collection-name]
-	{:find-type type :query query :collection collection-name})
- 
 (def user-collection-name "users")
 (def user-validation-set (create-validation-set-for-user))
 
+(defn- create-query [type query]
+	{:find-type type 
+		:query query 
+		:collection user-collection-name})
+ 
 (defn save-user [user]
 	(when (valid? user-validation-set user) 
 		(db-insert user-collection-name user)))
 
 (defn find-user-by-id [id]
-	(db-find 
-		(create-query-details :find-one {:_id (ObjectId. id)} user-collection-name)))
+	(db-find (create-query :find-one {:_id (ObjectId. id)})))
 
 (defn find-user-by-email-and-pass [email pass]
-	(db-find 
-		(create-query-details :find-one {:email email :password pass} user-collection-name)))
+	(db-find (create-query :find-one {:email email :password pass})))
 
 (defn find-user-by-name [name]
-	(db-find (create-query-details :find-one {:name name} user-collection-name)))
+	(db-find (create-query :find-one {:name name})))
