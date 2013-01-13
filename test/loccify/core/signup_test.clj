b31b5db211a1f3beb63506053249550aeab20094
Jani Arvonen <jani.arvonen@gmail.com>
2013-01-13 15:54:41 +0200

    renamed facts a bit for clarity

--- test/loccify/core/signup_test.clj
+++ test/loccify/core/signup_test.clj
@@ -10,13 +10,13 @@
 
 (def user (new-user "teppo" "teppo@testaaja.fi" "secret" :email))
 
-(fact "should signup user when name and email are available"
+(fact "should signup a user when both name and email are available"
 	(signup user) => user
 	(provided (available-username? "teppo") => true)
 	(provided (available-email? "teppo@testaaja.fi") => true)
 	(provided (save-user user) => user))
 
-(fact "should not signup user when name is not available"
+(fact "should not signup a user when name is not available"
 	(signup user) => nil
 	(provided (available-username? "teppo") => false)
 	(provided (available-email? "teppo@testaaja.fi") => true)
