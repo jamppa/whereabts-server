890fbbfef63c067493eb8143e837c3b87ad8ef8e
Jani Arvonen <jani.arvonen@gmail.com>
2013-01-13 14:55:25 +0200

    should not signup a user when name is not available

--- test/loccify/core/signup_test.clj
+++ test/loccify/core/signup_test.clj
@@ -15,3 +15,9 @@
 	(provided (available-username? "teppo") => true)
 	(provided (available-email? "teppo@testaaja.fi") => true)
 	(provided (save-user user) => user))
+
+(fact "should not signup user when name is not available"
+	(signup user) => nil
+	(provided (available-username? "teppo") => false)
+	(provided (available-email? "teppo@testaaja.fi") => true)
+	(provided (save-user user) => nil :times 0))
