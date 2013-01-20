13d49371ec171ab62be59b232ba05794b2f5e751
Jani Arvonen <jani.arvonen@gmail.com>
2013-01-20 21:09:11 +0200

    formatted project.clj for clarity

--- project.clj
+++ project.clj
@@ -1,15 +1,16 @@
 (defproject loccify "0.1.0-SNAPSHOT"
-            :description "Server for location based messaging service"
-            :dependencies [[org.clojure/clojure "1.4.0"]
-                           [compojure "1.1.3"]
-                           [ring-middleware-format "0.2.2"]
-                           [ring/ring-json "0.1.2"]
-                           [ring-http-basic-auth "0.0.1"]
-                           [com.novemberain/monger "1.3.4"]
-                           [midje "1.4.0"]]
-            :plugins [[lein-ring "0.7.5"]]
-            :ring {:handler loccify.server/server
-            		 :init loccify.server/init-db-connection}
-            :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}}
-            :aot [loccify.exception.SignUpException])
+   :description "Server for location based messaging service"
+   :dependencies 
+      [[org.clojure/clojure "1.4.0"]
+      [compojure "1.1.3"]
+      [ring-middleware-format "0.2.2"]
+      [ring/ring-json "0.1.2"]
+      [ring-http-basic-auth "0.0.1"]
+      [com.novemberain/monger "1.3.4"]
+      [midje "1.4.0"]]
+   :plugins [[lein-ring "0.7.5"]]
+   :ring {:handler loccify.server/server
+            :init loccify.server/init-db-connection}
+   :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}}
+   :aot [loccify.exception.SignUpException])
 
