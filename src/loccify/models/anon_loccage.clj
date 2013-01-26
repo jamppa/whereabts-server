2aa6883199d9fa9216de042aff79d20957cd6c53
Jani Arvonen <jani.arvonen@gmail.com>
2013-01-26 18:27:47 +0200

    should save valid anonymous loccage

--- src/loccify/models/anon_loccage.clj
+++ src/loccify/models/anon_loccage.clj
@@ -1,8 +1,21 @@
 (ns loccify.models.anon-loccage
 	(:use
 		[loccify.db]
-		[loccify.models.util]))
+		[loccify.models.util]
+		[validateur.validation]))
+
+(def anon-loccage-validation
+	(validation-set
+		(presence-of :message)
+		(presence-of :nick)
+		(presence-of :loc)
+		(presence-of :created-at)))
 
 (defn find-anon-loccage-by-id [id-str]
 	(db-find
 		(db-find-details :find-one "anon-loccages" {:_id (obj-id id-str)})))
+
+(defn save-anon-loccage [loccage]
+	(let [new-loccage (created-now loccage)]
+		(when (valid? anon-loccage-validation new-loccage)
+			(db-insert "anon-loccages" new-loccage))))
--- test/loccify/models/anon_loccage_test.clj
+++ test/loccify/models/anon_loccage_test.clj
@@ -4,7 +4,13 @@
 		[loccify.models.anon-loccage]
 		[loccify.db-helper]))
 
+(def valid-anon-loccage {:message "msg" :nick "Cool Guy" :loc [45.1 56.4]})
+
 (background (before :facts (setup-test-db)))
 
 (fact "should find anonymous loccage by id"
 	(find-anon-loccage-by-id "509d513f61395f0ebbd5e36a") => test-anon-loccage-a)
+
+(fact "should save valid anonymous loccage"
+	(let [saved-loccage (save-anon-loccage valid-anon-loccage)]
+		(find-anon-loccage-by-id (.toString (:_id saved-loccage))) => saved-loccage))
