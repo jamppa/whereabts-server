4c2cca7939683a8136508b844af68dc08be3fe7a
Jani Arvonen <jani.arvonen@gmail.com>
2013-01-26 18:46:15 +0200

    should not save invalid anonymous loccage

--- test/loccify/models/anon_loccage_test.clj
+++ test/loccify/models/anon_loccage_test.clj
@@ -6,6 +6,7 @@
 		[loccify.models.util]))
 
 (def valid-anon-loccage {:message "msg" :nick "Cool Guy" :loc [45.1 56.4]})
+(def invalid-anon-loccage {:message "asdasd" :loc [12.2 34.4]})
 
 (background (before :facts (setup-test-db)))
 
@@ -15,3 +16,6 @@
 (fact "should save valid anonymous loccage"
 	(let [saved-loccage (save-anon-loccage valid-anon-loccage)]
 		(find-anon-loccage-by-id (obj-id-as-str saved-loccage)) => saved-loccage))
+
+(fact "should not save invalid anonymous loccage"
+	(save-anon-loccage invalid-anon-loccage) => nil)
