4b4fd7ff2d10ec7650a8438557b5b33d35a89174
Jani Arvonen <jani.arvonen@gmail.com>
2013-01-26 18:43:52 +0200

    added var to hold name of the collection for anonymous loccages

--- src/loccify/models/anon_loccage.clj
+++ src/loccify/models/anon_loccage.clj
@@ -4,6 +4,7 @@
 		[loccify.models.util]
 		[validateur.validation]))
 
+(def anon-loccage-col "anon-loccages")
 (def anon-loccage-validation
 	(validation-set
 		(presence-of :message)
@@ -13,9 +14,9 @@
 
 (defn find-anon-loccage-by-id [id-str]
 	(db-find
-		(db-find-details :find-one "anon-loccages" {:_id (obj-id id-str)})))
+		(db-find-details :find-one anon-loccage-col {:_id (obj-id id-str)})))
 
 (defn save-anon-loccage [loccage]
 	(let [new-loccage (created-now loccage)]
 		(when (valid? anon-loccage-validation new-loccage)
-			(db-insert "anon-loccages" new-loccage))))
+			(db-insert anon-loccage-col new-loccage))))
