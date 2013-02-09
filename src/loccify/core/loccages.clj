caaf6c0979653429c90e078f1c60e32de137ef90
Jani Arvonen <jani.arvonen@gmail.com>
2013-02-09 15:47:58 +0200

    should find all loccages by bouding box

--- src/loccify/core/loccages.clj
+++ src/loccify/core/loccages.clj
@@ -3,3 +3,10 @@
 		[loccify.models.loccage]
 		[loccify.models.anon-loccage]))
 
+(defn- all-loccages [loccages anon-loccages]
+	{:loccages loccages :anon-loccages anon-loccages})
+
+(defn find-all-loccages-by-bbox [bbox]
+	(let [loccages (find-loccages-by-bbox bbox)
+		anon-loccages (find-anon-loccages-by-bbox bbox)]
+		(all-loccages loccages anon-loccages)))
--- test/loccify/core/loccages_test.clj
+++ test/loccify/core/loccages_test.clj
@@ -1,5 +1,18 @@
 (ns loccify.core.loccages-test
 	(:use
 		[midje.sweet]
-		[loccify.core.loccages]))
+		[loccify.models.loccage]
+		[loccify.models.anon-loccage]
+		[loccify.core.loccages]
+		[loccify.util.geo]))
 
+(def loccages [{:message ""}])
+(def anon-loccages [{:message ""}])
+(def expected-all-loccages {:loccages loccages :anon-loccages anon-loccages})
+
+(def bbox (bounding-box [1 1] [1 1]))
+
+(fact "should find all loccages by bounding box"
+	(find-all-loccages-by-bbox bbox) => expected-all-loccages
+	(provided (find-loccages-by-bbox bbox) => loccages :times 1)
+	(provided (find-anon-loccages-by-bbox bbox) => anon-loccages :times 1))
