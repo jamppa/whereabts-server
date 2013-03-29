cc5f7e4b46f4a505846544524ba87ccbdac723e4
Jani Arvonen <jani.arvonen@gmail.com>
2013-03-29 22:34:16 +0200

    updated midje and added it to dev dependecies

--- project.clj
+++ project.clj
@@ -6,10 +6,9 @@
       [ring-middleware-format "0.2.2"]
       [ring/ring-json "0.1.2"]
       [ring-http-basic-auth "0.0.1"]
-      [com.novemberain/monger "1.3.4"]
-      [midje "1.4.0"]]
+      [com.novemberain/monger "1.3.4"]]
    :plugins [[lein-ring "0.8.3"]]
    :ring {:handler loccify.server/server
             :init loccify.server/init-db-connection}
-   :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+   :profiles {:dev {:dependencies [[ring-mock "0.1.3"] [midje "1.5.1"]]}})
 
