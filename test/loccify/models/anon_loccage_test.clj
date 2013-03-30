8b25e545a32bdeb1dd49035d1e30292a2b75e120
Jani Arvonen <jani.arvonen@gmail.com>
2013-03-30 19:40:24 +0200

    renamed facts for clarity; no more loccages

--- test/loccify/models/anon_loccage_test.clj
+++ test/loccify/models/anon_loccage_test.clj
@@ -12,21 +12,21 @@
 
 (background (before :facts (setup-test-db)))
 
-(fact "should find anonymous loccage by id"
+(fact "should find anonymous message by id"
 	(find-anon-loccage-by-id "509d513f61395f0ebbd5e36a") => test-anon-loccage-a)
 
-(fact "should not find anonymous loccage by id when one does not exist"
+(fact "should not find anonymous message by id when one does not exist"
 	(find-anon-loccage-by-id "509d513f61395f0ebbd5e666") => nil)
 
-(fact "should save valid anonymous loccage"
+(fact "should save valid anonymous message"
 	(let [saved-loccage (save-anon-loccage valid-anon-loccage)]
 		(find-anon-loccage-by-id (obj-id-as-str saved-loccage)) => saved-loccage))
 
-(fact "should not save invalid anonymous loccage"
+(fact "should not save invalid anonymous message"
 	(save-anon-loccage invalid-anon-loccage) => nil)
 
-(fact "should find anonymous loccages by bounding box sorted by creation time"
+(fact "should find anonymous messages by bounding box sorted by creation time"
 	(find-anon-loccages-by-bbox (bounding-box [0 0] [10 10])) => [test-anon-loccage-b test-anon-loccage-a])
 
-(fact "should not find anonymous loccages by bounding box when there isnt any"
+(fact "should not find anonymous messages by bounding box when there isnt any"
 	(find-anon-loccages-by-bbox (bounding-box [45 34] [23 56])) => [])
