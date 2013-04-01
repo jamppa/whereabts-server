39bb5b627ea0d76173f8c5d8106b0c8c171d9055
Jani Arvonen <jani.arvonen@gmail.com>
2013-04-01 13:57:54 +0300

    refactor: renamed api/loccages_test to api/messages_api_test and changed mapping

--- src/loccify/api/messages_api.clj
+++ src/loccify/api/messages_api.clj
@@ -7,7 +7,7 @@
 
 (defroutes loccage-routes
 
-	(GET "/loccages/:ll-lon/:ll-lat/:ur-lon/:ur-lat" [ll-lon ll-lat ur-lon ur-lat]
+	(GET "/messages/:ll-lon/:ll-lat/:ur-lon/:ur-lat" [ll-lon ll-lat ur-lon ur-lat]
 		(let [ll-vec [(read-string ll-lon) (read-string ll-lat)]
 			ur-vec [(read-string ur-lon) (read-string ur-lat)]]
 		(response (find-all-messages-by-bbox (bounding-box ll-vec ur-vec)))))
---
+++ test/loccify/api/messages_api_test.clj
@@ -0,0 +1,16 @@
+(ns loccify.api.messages-api-test
+	(:use
+		[loccify.api.messages-api]
+		[loccify.core.messages]
+		[midje.sweet]
+		[ring.mock.request]
+		[loccify.api-helper]
+		[loccify.util.geo]))
+
+(def bbox (bounding-box [1.23 1.23] [5.0 5.0]))
+(def messages {:messages [] :anon-messages []})
+(def expected-res-for-messages (expected-res 200 messages))
+
+(fact "should GET all messages inside bounding box"
+	(loccage-routes (request :get "/messages/1.23/1.23/5.0/5.0")) => expected-res-for-messages
+	(provided (find-all-messages-by-bbox bbox) => messages :times 1))
