1881e21e6c9f53c74ea209ec3823aa59226c9483
Jani Arvonen <jani.arvonen@gmail.com>
2013-04-09 14:38:31 +0300

    should response with HTTP Unauthorized when trying to POST message with invalid credentials

--- test/whereabts/functional/messages_api_functest.clj
+++ test/whereabts/functional/messages_api_functest.clj
@@ -11,10 +11,14 @@
 (def message-as-json (json/write-str {:nick "teppo" :message "yey, cool things!" :loc [34.122 45.434]}))
 (def invalid-message-as-json (json/write-str {:message "yey, cool things!" :loc [34.122 45.434]}))
 
-(fact "should POST new message to service as an anonymous whereabts user" :functional
+(fact "should response with HTTP Created when POST new message to service as an anonymous whereabts user" :functional
 	(:status (http/post whereabts-api-messages 
 		(whereabts-api-request-anon message-as-json))) => 201)
 
 (fact "should response with HTTP Bad Request when trying to POST invalid message" :functional
 	(:status (http/post whereabts-api-messages
 		(whereabts-api-request-anon invalid-message-as-json))) => 400)
+
+(fact "should response with HTTP Unauthorized when trying to POST message with invalid credentials" :functional
+	(:status (http/post whereabts-api-messages
+		(whereabts-api-request ["invalid@user.com" "blaah"] message-as-json))) => 401)
