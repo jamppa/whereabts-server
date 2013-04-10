b7bd212b34dfc8b9c523f4655f900310364e3787
Jani Arvonen <jani.arvonen@gmail.com>
2013-04-10 11:57:55 +0300

    should response with HTTP Created when POST new message with emptu title as an anon user

--- test/whereabts/functional/messages_api_functest.clj
+++ test/whereabts/functional/messages_api_functest.clj
@@ -8,10 +8,20 @@
 
 (def whereabts-api-messages (str whereabts-api-testsrv "/messages"))
 
-(def message-as-json (json/write-str {:nick "teppo" :title "cool title!" :message "yey, cool things!" :loc [34.122 45.434]}))
-(def invalid-message-as-json (json/write-str {:message "yey, cool things!" :loc [34.122 45.434]}))
+(def message-as-json 
+	(json/write-str {:nick "teppo" :title "cool title!" :message "yey, cool things!" :loc [34.122 45.434]}))
 
-(fact "should response with HTTP Created when POST new message to service as an anonymous whereabts user" :functional
+(def message-with-empty-title-as-json
+	(json/write-str {:nick "teppo" :title "" :message "yey, cool things!" :loc [34.122 45.434]}))
+
+(def invalid-message-as-json 
+	(json/write-str {:message "yey, cool things!" :loc [34.122 45.434]}))
+
+(fact "should response with HTTP Created when POST new message as an anonymous whereabts user" :functional
+	(:status (http/post whereabts-api-messages 
+		(whereabts-api-request-anon message-as-json))) => 201)
+
+(fact "should reponse with HTTP Created when POST new message with empty title as an anonymous user" :functional
 	(:status (http/post whereabts-api-messages 
 		(whereabts-api-request-anon message-as-json))) => 201)
 
