e64aeed4f28159ba413a6adfd60553b2b5f85c2f
Jani Arvonen <jani.arvonen@gmail.com>
2013-04-11 16:24:59 +0300

    should response with HTTP OK when GETting all messages by bounding box

--- test/whereabts/functional/messages_api_functest.clj
+++ test/whereabts/functional/messages_api_functest.clj
@@ -7,6 +7,8 @@
 		[clojure.data.json :as json]))
 
 (def whereabts-api-messages (str whereabts-api-testsrv "/messages"))
+(defn- whereabts-api-messages-by-bbox [ll-lon ll-lat ur-lon ur-lat]
+	(str whereabts-api-messages "/" ll-lon "/" ll-lat "/" ur-lon "/" ur-lat))
 
 (def message-as-json 
 	(json/write-str {:nick "teppo" :title "cool title!" :message "yey, cool things!" :loc [34.122 45.434]}))
@@ -32,3 +34,7 @@
 (fact "should response with HTTP Unauthorized when trying to POST message with invalid credentials" :functional
 	(:status (http/post whereabts-api-messages
 		(whereabts-api-request ["invalid@user.com" "blaah"] message-as-json))) => 401)
+
+(fact "should response with HTTP OK when GETing all messages by bounding box" :functional
+	(:status (http/get (whereabts-api-messages-by-bbox 24.987 60.255 24.989 60.260)
+		(whereabts-api-request-anon ""))) => 200)
