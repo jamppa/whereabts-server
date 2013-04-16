32e68533c4736490261604df94dedc329ff82e80
Jani Arvonen <jani.arvonen@gmail.com>
2013-04-16 16:09:42 +0300

    cleaned test a bit for clarity

--- test/whereabts/models/message_test.clj
+++ test/whereabts/models/message_test.clj
@@ -7,25 +7,23 @@
 		[whereabts.util.geo]
 		[whereabts.db-test-fixtures]))
 
-(def anon-message {
+(def message {
 	:title "My Cool Message" 
 	:message "This is the content of My Cool Message" 
 	:nick "Cool Guy"
 	:loc {:lon 45.1 :lat 56.4}
 	:created-at (System/currentTimeMillis)})
 
-(def anon-message-after-copy (merge anon-message {:loc [45.1 56.4]}))
-(def anon-message-with-obj-id (with-obj-id anon-message))
-(def msg-with-empty-title (merge anon-message {:title ""}))
-(def msg-title-too-long 
-	(merge anon-message {:title "this is title text that is unfortunately too looooooong"}))
-(def msg-too-long
-	(merge anon-message {:message (clojure.string/join "" (repeat 251 "s"))}))
-(def msg-nick-too-long (merge anon-message {:nick "Termitekiller12345678"}))
-(def msg-missing-title (dissoc anon-message :title))
-(def msg-extra-kv (merge anon-message {:some "bullshit"}))
-(def msg-loc-erronous (merge anon-message {:loc "im invalid"}))
-(def msg-lon-lat-str (merge anon-message {:loc {:lon "123.323" :lat "blaa"}}))
+(def msg-after-copy (merge message {:loc [45.1 56.4]}))
+(def msg-with-obj-id (with-obj-id message))
+(def msg-with-empty-title (merge message {:title ""}))
+(def msg-title-too-long (merge message {:title "this is title text that is unfortunately too looooooong"}))
+(def msg-too-long (merge message {:message (clojure.string/join "" (repeat 251 "s"))}))
+(def msg-nick-too-long (merge message {:nick "Termitekiller12345678"}))
+(def msg-missing-title (dissoc message :title))
+(def msg-extra-kv (merge message {:some "bullshit"}))
+(def msg-loc-erronous (merge message {:loc "im invalid"}))
+(def msg-lon-lat-str (merge message {:loc {:lon "123.323" :lat "blaa"}}))
 
 (background (before :facts (setup-test-db)))
 
@@ -36,7 +34,7 @@
 	(find-message-by-id "509d513f61395f0ebbd5e666") => nil)
 
 (fact "should save valid anonymous message"
-	(let [saved-message (save-message anon-message)]
+	(let [saved-message (save-message message)]
 		(find-message-by-id (obj-id-as-str saved-message)) => saved-message))
 
 (fact "should not save invalid anonymous message missing title but throw IllegalArgumentException"
@@ -68,11 +66,11 @@
 		(find-message-by-id (obj-id-as-str saved-message)) => saved-message))
 
 (fact "should compactify anonymous message extracting short-message"
-	(keys (compactify-message anon-message-with-obj-id)) => '(:_id :loc :short-message :created-at)
-	(provided (short-message anon-message-with-obj-id) => "short message" :times 1))
+	(keys (compactify-message msg-with-obj-id)) => '(:_id :loc :short-message :created-at)
+	(provided (short-message msg-with-obj-id) => "short message" :times 1))
 
 (fact "should create a copy of anonymous message"
-	(new-message anon-message) => anon-message-after-copy)
+	(new-message message) => msg-after-copy)
 
 (fact "should ignore uninterested keys when copying new anonymous message"
-	(new-message msg-extra-kv) => anon-message-after-copy)
+	(new-message msg-extra-kv) => msg-after-copy)
