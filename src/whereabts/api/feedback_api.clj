5d800abd58de51475c0e4a8aa2776926b611970a
Jani Arvonen <jani.arvonen@gmail.com>
2013-04-24 20:18:36 +0300

    should POST new feedback-message

--- src/whereabts/api/feedback_api.clj
+++ src/whereabts/api/feedback_api.clj
@@ -1,1 +1,13 @@
-(ns whereabts.api.feedback-api)
+(ns whereabts.api.feedback-api
+	(:use
+		[whereabts.core.feedback]
+		[compojure.core]
+		[ring.util.response]))
+
+(defroutes feedback-api-routes
+
+	(POST "/feedbacks" [:as req]
+		(-> (response (save-new-feedback (:body req)))
+			(status 201)))
+
+	)
--- test/whereabts/api/feedback_api_test.clj
+++ test/whereabts/api/feedback_api_test.clj
@@ -1,2 +1,16 @@
 (ns whereabts.api.feedback-api-test
-	)
+	(:use
+		[midje.sweet]
+		[ring.mock.request]
+		[whereabts.api-helper]
+		[whereabts.api.feedback-api]
+		[whereabts.core.feedback]))
+
+(def feedback-payload {})
+(def saved-feedback {:_id "123123"})
+(def successful-feedback-post-response (expected-res 201 saved-feedback))
+
+(fact "should POST new feedback message"
+	(feedback-api-routes 
+		(whereabts-request :post "/feedbacks" feedback-payload)) => successful-feedback-post-response
+	(provided (save-new-feedback feedback-payload) => saved-feedback :times 1))
