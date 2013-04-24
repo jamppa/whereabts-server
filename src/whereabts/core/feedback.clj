e1e594b43efd2c9701b40d1d18a07bb072d0f9bd
Jani Arvonen <jani.arvonen@gmail.com>
2013-04-24 15:03:20 +0300

    should save new feedback with current timestamp

--- src/whereabts/core/feedback.clj
+++ src/whereabts/core/feedback.clj
@@ -1,3 +1,8 @@
 (ns whereabts.core.feedback
     (:use
-        [whereabts.models.feedback]))
+        [whereabts.models.feedback]
+        [whereabts.models.util]))
+
+(defn save-new-feedback [new-feedback]
+    (let [the-feedback (created-now new-feedback)]
+        (save-feedback the-feedback)))
--- test/whereabts/core/feedback_test.clj
+++ test/whereabts/core/feedback_test.clj
@@ -1,4 +1,14 @@
 (ns whereabts.core.feedback-test
 	(:use 
 		[whereabts.core.feedback]
+		[whereabts.models.feedback]
+		[whereabts.models.util]
 		[midje.sweet]))
+
+(def new-feedback {})
+(def feedback-created-now {:created-at 123123})
+
+(fact "should save new feedback with current timestamp"
+	(save-new-feedback new-feedback) => feedback-created-now
+	(provided (created-now new-feedback) => feedback-created-now :times 1)
+	(provided (save-feedback feedback-created-now) => feedback-created-now :times 1))
