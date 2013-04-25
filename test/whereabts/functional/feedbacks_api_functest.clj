2c5ba816ee34176f60fc53cb360a86d7c087a76e
Jani Arvonen <jani.arvonen@gmail.com>
2013-04-25 11:00:39 +0300

    should POST invalid feeback-payload but response with HTTP Bad Request

--- test/whereabts/functional/feedbacks_api_functest.clj
+++ test/whereabts/functional/feedbacks_api_functest.clj
@@ -9,10 +9,15 @@
 (def whereabts-feedbacks-api (str whereabts-api-testsrv "/feedbacks"))
 (def feedback-payload
 	(json/write-str {:vote 0 :message "cool app guys!"}))
+(def invalid-feedback-payload
+	(json/write-str {:vote 0}))
+
+(defn- post-feedback-as-anon [payload]
+	(http/post whereabts-feedbacks-api
+		(whereabts-api-request-anon payload)))
 
 (fact "should POST feedback-payload and response with HTTP Created" :functional
-	(:status (http/post whereabts-feedbacks-api
-		(whereabts-api-request-anon feedback-payload))) => 201)
+	(:status (post-feedback-as-anon feedback-payload)) => 201)
 
-
-
+(fact "should POST invalid feedback-payload and response with HTTP Bad Request" :functional
+	(:status (post-feedback-as-anon invalid-feedback-payload)) => 400)
