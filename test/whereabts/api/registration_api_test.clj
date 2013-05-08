45b98cd99b012b401713a5a4ec4d0bb7ebb41779
Jani Arvonen <jani.arvonen@gmail.com>
2013-05-08 11:57:08 +0300

    should throw exception when POSTing invalid anonymous user details

--- test/whereabts/api/registration_api_test.clj
+++ test/whereabts/api/registration_api_test.clj
@@ -7,9 +7,15 @@
 		[midje.sweet]))
 
 (def payload {:user-uuid "123-abc" :email "anonymous@whereabts.com"})
+(def invalid-payload {:invalid "payload"})
 (def successful-response (expected-res 201 payload))
 
 (fact "should POST anonymous user details"
 	(registration-api-routes
 		(whereabts-request :post "/anonymousregistration" payload)) => successful-response
 	(provided (register-anonymous-user payload) => payload :times 1))
+
+(fact "should throw exception when POSTing invalid anonymous user details"
+	(registration-api-routes
+		(whereabts-request :post "/anonymousregistration" invalid-payload)) => (throws IllegalArgumentException)
+	(provided (register-anonymous-user {:user-uuid nil :email nil}) =throws=> (IllegalArgumentException. "invalid!")))
