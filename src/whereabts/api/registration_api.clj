b120a33463c73486d7b001d0898cfc563dffc8a7
Jani Arvonen <jani.arvonen@gmail.com>
2013-05-08 11:45:50 +0300

    should POST anonymous user registration details

--- src/whereabts/api/registration_api.clj
+++ src/whereabts/api/registration_api.clj
@@ -1,1 +1,19 @@
-(ns whereabts.api.registration-api)
+(ns whereabts.api.registration-api
+	(:use
+		[whereabts.core.registration]
+		[compojure.core]
+		[ring.util.response]
+		[clojure.walk :only [keywordize-keys]]))
+
+(defn- extract-user [req]
+	(let [body (keywordize-keys (:body req))]
+		{:user-uuid (:user-uuid body) :email (:email body)}))
+
+(defroutes registration-api-routes
+
+	(POST "/anonymousregistration" [:as req]
+		(let [user (extract-user req)]
+			(-> (response (register-anonymous-user user))
+				(status 201))))
+
+	)
--- test/whereabts/api/registration_api_test.clj
+++ test/whereabts/api/registration_api_test.clj
@@ -1,4 +1,15 @@
 (ns whereabts.api.registration-api-test
 	(:use
-		[whereabts.api.registration]
+		[whereabts.core.registration]
+		[whereabts.api.registration-api]
+		[whereabts.api-helper]
+		[ring.mock.request]
 		[midje.sweet]))
+
+(def payload {:user-uuid "123-abc" :email "anonymous@whereabts.com"})
+(def successful-response (expected-res 201 payload))
+
+(fact "should POST anonymous user details"
+	(registration-api-routes
+		(whereabts-request :post "/anonymousregistration" payload)) => successful-response
+	(provided (register-anonymous-user payload) => payload :times 1))
