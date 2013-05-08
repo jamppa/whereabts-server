3c2d4e068ccf8772bf51a8d7cd2e74740c206e27
Jani Arvonen <jani.arvonen@gmail.com>
2013-05-08 13:28:38 +0300

    should response with bad request when posting invalid formatted anonymous user registration payload

--- test/whereabts/functional/registration_api_functest.clj
+++ test/whereabts/functional/registration_api_functest.clj
@@ -10,6 +10,9 @@
 (def registration-payload 
 	(json/write-str {:user-uuid (.toString (UUID/randomUUID)) :email "anonymous@whereabts.com"}))
 
+(def invalid-registration-payload
+	(json/write-str {:invalid "value" :email "anonymous@whereabts.com"}))
+
 (def anonymous-registration-api 
 	(str whereabts-api-testsrv "/anonymousregistration"))
 
@@ -17,5 +20,8 @@
 	(http/post anonymous-registration-api
 		(whereabts-api-request-anon payload)))
 
-(fact "should POST valid anonymous user registration payload and response with HTTP Created" :functional
+(fact "should response with HTTP Created When POSTing valid anonymous user registration payload" :functional
 	(:status (post-as-anon registration-payload)) => 201)
+
+(fact "should response with Bad Request When POSTing invalid formatted anonymous user registration payload" :functional
+	(:status (post-as-anon invalid-registration-payload)) => 400)
