de3e1befbc451016ef53311c2f82e405df21097a
Jani Arvonen <jani.arvonen@gmail.com>
2013-05-11 22:56:55 +0300

    should not authenticate and instead return nil when anonymous-user email is wrong

--- test/whereabts/core/auth_test.clj
+++ test/whereabts/core/auth_test.clj
@@ -4,7 +4,8 @@
 		[whereabts.models.anonymous-user]
 		[midje.sweet]))
 
-(def user {})
-
 (fact "should authenticate by returning anonymous system user when email and pass match"
 	(authenticate "anonymous@whereabts.com" "ae129325a4db22faab7771f10b39a8af") => anonymous-whereabts-user)
+
+(fact "should not authenticate and instead return nil when anonymous-user email is wrong"
+	(authenticate "wrong@email.com" "ae129325a4db22faab7771f10b39a8af") => nil)
