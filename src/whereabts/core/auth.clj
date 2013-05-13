43ba61e735965090c1c4d74cd291ffeea2149189
Jani Arvonen <jani.arvonen@gmail.com>
2013-05-13 15:38:43 +0300

    should update last seen timestamp when authenticating now

--- src/whereabts/core/auth.clj
+++ src/whereabts/core/auth.clj
@@ -1,9 +1,15 @@
 (ns whereabts.core.auth
-	(:use [whereabts.models.anonymous-user]))
+	(:use 
+		[whereabts.models.anonymous-user]
+		[whereabts.models.util]))
 
 (defn- anon-whereabts-user? [email pwd]
 	(and 
 		(= email (anonymous-whereabts-user :email)) (= pwd (anonymous-whereabts-user :user-id))))
+
+(defn authenticated-now [user]
+	(when user
+		(last-seen-now user)))
 
 (defn authenticate-whereabts-anon-user [email pwd]
 	(if (anon-whereabts-user? email pwd) 
@@ -11,4 +17,5 @@
 		nil))
 
 (defn authenticate [email uuid]
-	(find-anonymous-user (by-uuid-and-email uuid email)))
+	(authenticated-now 
+		(find-anonymous-user (by-uuid-and-email uuid email))))
--- test/whereabts/core/auth_test.clj
+++ test/whereabts/core/auth_test.clj
@@ -2,9 +2,12 @@
 	(:use 
 		[whereabts.core.auth]
 		[whereabts.models.anonymous-user]
+		[whereabts.models.util]
 		[midje.sweet]))
 
 (def anonymous-user {:email "anonymous@whereabts.com" :user-uuid "123abc"})
+(def anonymous-user-authenticated-now
+	(merge anonymous-user {:last-seen-at (System/currentTimeMillis)}))
 
 (fact "should authenticate as whereabts user when email and pass match"
 	(authenticate-whereabts-anon-user "anonymous@whereabts.com" "ae129325a4db22faab7771f10b39a8af") => anonymous-whereabts-user)
@@ -17,8 +20,17 @@
 
 (fact "should authenticate as anonymous user when one is found by email and uuid"
 	(authenticate "anonymous@whereabts.com" "123abc") => anonymous-user
-	(provided (find-anonymous-user anonymous-user) => anonymous-user :times 1))
+	(provided (find-anonymous-user anonymous-user) => anonymous-user :times 1)
+	(provided (authenticated-now anonymous-user) => anonymous-user :times 1))
 
 (fact "should not authenticate as anonymous user when one is not found by email and uuid"
 	(authenticate "anonymous@whereabts.com" "123abc") => nil
 	(provided (find-anonymous-user anonymous-user) => nil :times 1))
+
+(fact "should update last seen timestamp and save user when authenticating now"
+	(authenticated-now anonymous-user) => anonymous-user-authenticated-now
+	(provided (last-seen-now anonymous-user) => anonymous-user-authenticated-now :times 1))
+
+(fact "should not update last seen timestamp when now authenticating nil user"
+	(authenticated-now nil) => nil
+	(provided (last-seen-now nil) => nil :times 0))
