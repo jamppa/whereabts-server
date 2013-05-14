f894bdb06b06726d7143e667472cef9f33aeaad2
Jani Arvonen <jani.arvonen@gmail.com>
2013-05-14 13:55:28 +0300

    feedbacks-api now also required anonymous user role

--- src/whereabts/api/feedbacks_api.clj
+++ src/whereabts/api/feedbacks_api.clj
@@ -1,6 +1,7 @@
 (ns whereabts.api.feedbacks-api
 	(:use
 		[whereabts.core.feedbacks]
+		[whereabts.api.api-utils]
 		[compojure.core]
 		[ring.util.response]
 		[clojure.walk :only [keywordize-keys]]))
@@ -8,8 +9,9 @@
 (defroutes feedbacks-api-routes
 
 	(POST "/feedbacks" [:as req]
-		(let [feedback (keywordize-keys (:body req))]
-		(-> (response (save-new-feedback feedback))
-			(status 201))))
+		(with-role req "anonymous"
+			(let [feedback (keywordize-keys (:body req))]
+			(-> (response (save-new-feedback feedback))
+				(status 201)))))
 
 	)
