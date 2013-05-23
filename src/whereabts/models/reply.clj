418e4980e9a20fd8848164eee098923cb5dc0cb4
Jani Arvonen <jani.arvonen@gmail.com>
2013-05-23 20:14:38 +0300

    should not save invalid reply message missing message id

--- src/whereabts/models/reply.clj
+++ src/whereabts/models/reply.clj
@@ -1,9 +1,18 @@
 (ns whereabts.models.reply
 	(:use
 		[whereabts.db]
-		[whereabts.models.util]))
+		[whereabts.models.util]
+		[validateur.validation]))
 
 (def replies-coll "replies")
+(def reply-validation
+	(validation-set
+		(presence-of :message_id)))
 
 (defn find-reply-by-id [id]
 	(db-find-one-by-id replies-coll (obj-id id)))
+
+(defn save-new-reply [reply]
+	(if (valid? reply-validation reply)
+	(db-insert replies-coll reply)
+	(throw (IllegalArgumentException. "Invalid reply!"))))
--- test/whereabts/models/reply_test.clj
+++ test/whereabts/models/reply_test.clj
@@ -2,16 +2,26 @@
 	(:use
 		[midje.sweet]
 		[whereabts.models.reply]
+		[whereabts.models.util]
 		[whereabts.db-helper]
 		[whereabts.db.reply-test-fixtures]))
 
 (def new-reply 
 	(merge (dissoc test-reply-a :_id) {:replymessage "Yyyeaaahhh!"}))
 
+(def reply-missing-msg-id (dissoc new-reply :message_id))
+
 (background (before :facts (setup-test-db)))
 
 (fact "should find reply by its id"
 	(find-reply-by-id "509d513f61395f0ebbd5e40a") => test-reply-a)
 
-; (fact "should save new reply message"
-; 	(let [saved (save-new-reply new-reply)]))
+(fact "should not find reply by its id when one does not exist"
+	(find-reply-by-id "509d513f61395f0ebbd5e666") => nil)
+
+(fact "should save new reply message"
+	(let [saved (save-new-reply new-reply)]
+		(find-reply-by-id (obj-id-as-str saved)) => saved))
+
+(fact "should not save invalid reply message missing message id"
+	(save-new-reply reply-missing-msg-id) => (throws IllegalArgumentException))
