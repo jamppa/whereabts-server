9a51bde14a6e6415b762ee0e9da14a8e9e53cdbc
Jani Arvonen <jani.arvonen@gmail.com>
2013-05-25 13:51:22 +0300

    should POST new reply to message from user

--- src/whereabts/api/replies_api.clj
+++ src/whereabts/api/replies_api.clj
@@ -1,1 +1,22 @@
-(ns whereabts.api.replies-api)
+(ns whereabts.api.replies-api
+	(:use
+		[whereabts.core.replies]
+		[whereabts.core.messages]
+		[whereabts.api.api-utils]
+		[compojure.core]
+		[ring.util.response]
+		[clojure.walk :only [keywordize-keys]]))
+
+(defroutes replies-api-routes
+
+	(POST "/messages/:id/replies" [id :as req]
+		(with-role req "anonymous"
+			(let [user (:basic-authentication req)
+				  reply-payload (keywordize-keys (:body req))
+				  message-to-reply (find-message id user)]
+				(-> (save-reply reply-payload user message-to-reply)
+					(response)
+					(status 201)))))
+
+
+)
--- test/whereabts/api/replies_api_test.clj
+++ test/whereabts/api/replies_api_test.clj
@@ -1,1 +1,24 @@
-(ns whereabts.api.replies-api-test)
+(ns whereabts.api.replies-api-test
+	(:use
+		[whereabts.api.replies-api]
+		[whereabts.core.replies]
+		[whereabts.core.messages]
+		[whereabts.core.with-util]
+		[midje.sweet]
+		[ring.mock.request]
+		[whereabts.api-helper]))
+
+(def new-reply-payload
+	{:nick "jamppa" :replymessage "yeah!"})
+
+(def message-to-reply {:_id 123})
+(def saved-reply 
+	(merge new-reply-payload {:_id 123 :user_id 123 :message_id 123}))
+
+(def expected-res-for-new-reply (expected-res 201 saved-reply))
+
+(fact "should POST new reply to message from user"
+	(replies-api-routes
+		(whereabts-request-as-anonymous-user :post "/messages/123abc/replies" new-reply-payload)) => expected-res-for-new-reply
+	(provided (find-message "123abc" anonymous-roled-user) => message-to-reply :times 1)
+	(provided (save-reply new-reply-payload anonymous-roled-user message-to-reply) => saved-reply :times 1))
