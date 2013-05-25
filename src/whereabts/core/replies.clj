8d2652f9a0374bcb6dad6f328ae1dda5bb214735
Jani Arvonen <jani.arvonen@gmail.com>
2013-05-25 12:09:17 +0300

    select keys before saving new reply

--- src/whereabts/core/replies.clj
+++ src/whereabts/core/replies.clj
@@ -4,9 +4,14 @@
 		[whereabts.models.util]
 		[whereabts.core.with-util]))
 
+(defn- new-reply [candidate]
+	(select-keys candidate
+		[:message_id :user_id :nick :replymessage :created-at]))
+
 (defn save-reply [reply user message]
 	(-> reply
 		(created-now)
 		(with-user user)
 		(with-message message)
+		(new-reply)
 		(save-new-reply)))
