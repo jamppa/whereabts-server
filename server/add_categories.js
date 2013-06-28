53b4640151448076371ea1cf2a1e1b1a6f4ef959
Jani Arvonen <jani.arvonen@gmail.com>
2013-06-28 12:32:25 +0300

    added  script for adding categories to db

---
+++ server/add_categories.js
@@ -0,0 +1,16 @@
+function addCategories() {
+	db.categories.remove();
+	db.categories.insert({"_id": 1, "key": "traffic" });
+	db.categories.insert({"_id": 2, "key": "food_drinks" });
+	db.categories.insert({"_id": 3, "key": "home_leisure" });
+	db.categories.insert({"_id": 4, "key": "travel" });
+	db.categories.insert({"_id": 5, "key": "nightlife" });
+	db.categories.insert({"_id": 6, "key": "sports_fitness" });
+	db.categories.insert({"_id": 7, "key": "activities" });
+	db.categories.insert({"_id": 8, "key": "shopping" });
+	db.categories.insert({"_id": 9, "key": "beauty" });
+	db.categories.insert({"_id": 10, "key": "happenings" });
+	db.categories.insert({"_id": 11, "key": "services" });
+}
+
+addCategories();
