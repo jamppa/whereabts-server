37606d2c47a25a049ecf7cb662b7982177c3879a
Jani Arvonen <jani.arvonen@gmail.com>
2013-07-02 10:59:09 +0300

    removed activities category

--- server/add_categories.js
+++ server/add_categories.js
@@ -5,12 +5,11 @@ function addCategories() {
 	db.categories.insert({"_id": 3, "key": "home_leisure" });
 	db.categories.insert({"_id": 4, "key": "travel" });
 	db.categories.insert({"_id": 5, "key": "nightlife" });
-	db.categories.insert({"_id": 6, "key": "sports_fitness" });
-	db.categories.insert({"_id": 7, "key": "activities" });
-	db.categories.insert({"_id": 8, "key": "shopping" });
-	db.categories.insert({"_id": 9, "key": "beauty" });
-	db.categories.insert({"_id": 10, "key": "happenings" });
-	db.categories.insert({"_id": 11, "key": "services" });
+	db.categories.insert({"_id": 6, "key": "sports_activities" });
+	db.categories.insert({"_id": 7, "key": "shopping" });
+	db.categories.insert({"_id": 8, "key": "beauty" });
+	db.categories.insert({"_id": 9, "key": "happenings" });
+	db.categories.insert({"_id": 10, "key": "services" });
 }
 
 addCategories();
