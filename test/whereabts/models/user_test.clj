9cca4ce3855a7a4fb6c593655ff96579a8692d1c
Jani Arvonen <jani.arvonen@gmail.com>
2013-07-05 16:25:22 +0300

    renamed facts for clarity

--- test/whereabts/models/user_test.clj
+++ test/whereabts/models/user_test.clj
@@ -25,38 +25,38 @@
 (fact "should find user by id"
 	(find-anonymous-user-by-id "509d513f61395f0ebbd5e38a") => anonymous-user-a)
 
-(fact "should save new anonymous user"
+(fact "should save new  user"
 	(let [saved-anonymous-user (save-anonymous-user new-user)]
 		(find-anonymous-user-by-id (obj-id-as-str saved-anonymous-user)) => saved-anonymous-user))
 
-(fact "should not save invalid anonymous user missing uuid"
+(fact "should not save invalid user missing uuid"
 	(save-anonymous-user new-user-missing-uuid) => (throws IllegalArgumentException))
 
-(fact "should not save invalid anonymous user with too short uuid"
+(fact "should not save invalid user with too short uuid"
 	(save-anonymous-user new-user-too-short-uuid) => (throws IllegalArgumentException))
 
-(fact "should not save invalid anonymous user with too long uuid"
+(fact "should not save invalid user with too long uuid"
 	(save-anonymous-user new-user-too-long-uuid) => (throws IllegalArgumentException))
 
-(fact "should not save invalid anonymous user missing email"
+(fact "should not save invalid user missing email"
 	(save-anonymous-user new-user-missing-email) => (throws IllegalArgumentException))
 
-(fact "should not save invalid anonymous user missing creation time"
+(fact "should not save invalid user missing creation time"
 	(save-anonymous-user new-user-missing-creationtime) => (throws IllegalArgumentException))
 
-(fact "should find anonymous user by uuid and email"
+(fact "should find user by uuid and email"
 	(find-anonymous-user 
 		(by-uuid-and-email "550e8400-e29b-41d4-a716-446655440000" "anonymous@whereabts.com")) => anonymous-user-a)
 
-(fact "should not find anonymous user with uuid with one that does not exist"
+(fact "should not find user with uuid with one that does not exist"
 	(find-anonymous-user 
 		(by-uuid-and-email "550e8400-e29b-41d4-a716-446655441234" "anonymous@whereabts.com")) => nil)
 
-(fact "should find and update anonymous user"
+(fact "should find and update user"
 	(let [found-user (find-anonymous-user-by-id "509d513f61395f0ebbd5e38a")
 		  updated-user (update-anonymous-user (merge found-user {:last-seen-at 1364642721971}))]
 		  (find-anonymous-user-by-id "509d513f61395f0ebbd5e38a") => (merge anonymous-user-a {:last-seen-at 1364642721971})))
 
-(fact "should throw exception when trying to update invalid anonymous user"
+(fact "should throw exception when trying to update invalid user"
 	(let [found-user (find-anonymous-user-by-id "509d513f61395f0ebbd5e38a")]
 		  (update-anonymous-user (merge found-user {:user-uuid "123-abc"})) => (throws IllegalArgumentException)))
