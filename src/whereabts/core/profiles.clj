e06fb7f8c301ceb542bdca4f5197507d53ead915
Jani Arvonen <jani.arvonen@gmail.com>
2013-08-12 14:58:30 +0300

    renamed stuff for clarity

--- src/whereabts/core/profiles.clj
+++ src/whereabts/core/profiles.clj
@@ -12,21 +12,21 @@
 (defn- has-profile? [user]
 	(contains? user :profile_id))
 
-(defn update-profile [user profile]
+(defn update-profile [user profile-details]
 	(let [profile-id (:profile_id user)
 		  user-id (:_id user)]
 	(save-profile 
-		(merge profile {:_id profile-id :user_id user-id}))))
+		(merge profile-details {:_id profile-id :user_id user-id}))))
 
-(defn create-profile [user profile]
-	(let [saved-profile (save-profile (with-user profile user))]
+(defn create-profile [user profile-details]
+	(let [saved-profile (save-profile (with-user profile-details user))]
 		(set-profile-for-user user saved-profile)
 	saved-profile))
 
-(defn save-user-profile [user profile]
+(defn save-user-profile [user profile-details]
 	(if (has-profile? user)
-		(update-profile user profile)
-		(create-profile user profile)))
+		(update-profile user profile-details)
+		(create-profile user profile-details)))
 
 (defn find-user-profile [user]
 	(when (not (has-profile? user)) (throw (WhereabtsResourceNotFoundException.)))
