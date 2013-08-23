d021b00f0a294f14faa5b537ed978681a6efb6fa
Jani Arvonen <jani.arvonen@gmail.com>
2013-08-23 16:33:06 +0300

    should return message with liked-status

--- src/whereabts/core/with_util.clj
+++ src/whereabts/core/with_util.clj
@@ -11,3 +11,8 @@
 	(if (= (id-as-str :user_id obj) (obj-id-as-str user))
 		(merge obj {:owns true})
 		(merge obj {:owns false})))
+
+(defn with-liked [message user]
+	(if (some #{(obj-id-as-str user)} (:likes message))
+		(merge message {:liked true})
+		(merge message {:liked false})))
--- test/whereabts/core/with_util_test.clj
+++ test/whereabts/core/with_util_test.clj
@@ -4,10 +4,10 @@
 		[whereabts.core.with-util]))
 
 (def obj {})
-(def user {:_id 123})
-(def other-user {:_id 234})
+(def user {:_id "123"})
+(def other-user {:_id "234"})
 (def obj-with-user (merge obj {:user_id (:_id user)}))
-(def message {:_id 456})
+(def message {:_id "456" :likes ["123"]})
 (def obj-with-message (merge obj {:message_id (:_id message)}))
 
 (fact "should return object with user id"
@@ -21,3 +21,13 @@
 
 (fact "should not add ownershipt to message when user does not own it"
 	(with-ownership obj-with-user other-user) => (merge obj-with-user {:owns false}))
+
+(fact "should return message with liked-status true when users id is in likes list"
+	(with-liked message user) => (merge message {:liked true}))
+
+(def message (merge message {:likes []}))
+(fact "should return message with liked-status false when users id is not in likes list"
+	(with-liked message user) => (merge message {:liked false}))
+
+(fact "should return message with liked-status false when likes list is nil"
+	(with-liked {} user) => {:liked false})
