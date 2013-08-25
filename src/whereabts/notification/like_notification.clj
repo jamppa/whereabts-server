11547bca18c6b7c1557b815edbd268f29295c00e
Jani Arvonen <jani.arvonen@gmail.com>
2013-08-25 13:40:25 +0300

    should prepare like message for channel and publish it

--- src/whereabts/notification/like_notification.clj
+++ src/whereabts/notification/like_notification.clj
@@ -1,4 +1,15 @@
 (ns whereabts.notification.like-notification
-	(:use whereabts.notification.core))
+	(:use 
+		whereabts.notification.core
+		whereabts.models.util))
 
 (def like-channel "message.likes")
+
+(defn prepare-like-message-for-channel [message user]
+	{
+		:user-id (obj-id-as-str user)
+		:message-id (obj-id-as-str message)})
+
+(defn publish-like-message [message user]
+	(publish-message like-channel 
+		(prepare-like-message-for-channel message user)))
--- test/whereabts/notification/like_notification_test.clj
+++ test/whereabts/notification/like_notification_test.clj
@@ -1,8 +1,26 @@
 (ns whereabts.notification.like-notification-test
 	(:use 
 		midje.sweet
-		whereabts.notification.like-notification))
+		whereabts.notification.like-notification
+		whereabts.notification.core
+		whereabts.db.test-fixtures
+		whereabts.db.user-test-fixtures))
 
 (fact "should define channel name for like notifications"
 	like-channel => "message.likes")
 
+(def liked-message test-message-a)
+(def user-who-liked test-user-a)
+(def like-channel-message {
+	:user-id (.toString (:_id user-who-liked))
+	:message-id (.toString (:_id liked-message))
+	})
+
+(fact "should prepare like message for channel"
+	(prepare-like-message-for-channel liked-message user-who-liked) => like-channel-message)
+
+(fact "should publish like message to channel"
+	(publish-like-message liked-message user-who-liked) => anything
+	(provided
+		(prepare-like-message-for-channel liked-message user-who-liked) => anything :times 1
+		(publish-message like-channel anything) => anything :times 1))
