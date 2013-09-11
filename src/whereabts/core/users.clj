1f1bd62266766775420399e76b51933ed0058dd0
Jani Arvonen <jani.arvonen@gmail.com>
2013-09-11 21:17:42 +0300

    simplified profile handling when saving new user

--- src/whereabts/core/users.clj
+++ src/whereabts/core/users.clj
@@ -7,31 +7,19 @@
 (defn with-email-role [user]
 	(merge user {:role "email"}))
 
-(defn with-no-profile [user]
-	(merge user {:profile_id ""}))
-
-(defn with-profile [user]
-	(if (contains? user :profile_id)
-		user
-		(with-no-profile user)))
-
 (defn save-user [user]
 	(-> user
 		(created-now)
 		(last-seen-now)
 		(with-email-role)
-		(save-new-user)
-		(with-no-profile)))
+		(save-new-user)))
 
 (defn find-user-by-email [email]
-	(if-let [user (find-user (by-email email))]
-		(with-profile user)))
+	(find-user (by-email email)))
 
 (defn update-gcm-for-user [user gcm]
 	(when (nil? gcm) (throw (IllegalArgumentException. "invalid gcm")))
-	(with-profile 
-		(update-user (merge user {:gcm-id gcm}))))
+	(update-user (merge user {:gcm-id gcm})))
 
 (defn set-profile-for-user [user profile]
-	(update-user 
-		(merge user {:profile_id (:_id profile)})))
+	(update-user (merge user {:profile_id (:_id profile)})))
--- test/whereabts/core/users_test.clj
+++ test/whereabts/core/users_test.clj
@@ -23,22 +23,22 @@
 (def user-without-profile (merge saved-user {:profile_id ""}))
 
 (fact "should save new user"
-	(save-user user) => user-without-profile
-		(provided (created-now user) => user-created-now :times 1)
-		(provided (last-seen-now user-created-now) => user-created-and-last-seen-now :times 1)
-		(provided (with-email-role user-created-and-last-seen-now) => user-with-email :times 1)
-		(provided (save-new-user user-with-email) => saved-user :times 1)
-		(provided (with-no-profile saved-user) => user-without-profile :times 1))
+	(save-user user) => saved-user
+		(provided 
+			(created-now user) => user-created-now :times 1
+			(last-seen-now user-created-now) => user-created-and-last-seen-now :times 1
+			(with-email-role user-created-and-last-seen-now) => user-with-email :times 1
+			(save-new-user user-with-email) => saved-user :times 1))
 
-(fact "should find user by email with profile"
-	(find-user-by-email "testman@testland.fi") => user-with-profile
-	(provided (find-user {:email "testman@testland.fi"}) => user-with-email :times 1)
-	(provided (with-profile user-with-email) => user-with-profile :times 1))
+(fact "should find user by email"
+	(find-user-by-email "testman@testland.fi") => saved-user
+	(provided 
+		(find-user {:email "testman@testland.fi"}) => saved-user :times 1))
 
 (fact "should update gcm for user"
 	(update-gcm-for-user user "aBCd") => user-with-gcm
-	(provided (update-user user-with-gcm) => user-with-gcm :times 1)
-	(provided (with-profile user-with-gcm) => user-with-gcm :times 1))
+	(provided 
+		(update-user user-with-gcm) => user-with-gcm :times 1))
 
 (fact "should not update gcm for user when gcm is nil"
 	(update-gcm-for-user user nil) => (throws IllegalArgumentException)
