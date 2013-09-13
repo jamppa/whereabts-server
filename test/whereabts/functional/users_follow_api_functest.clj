3d5dd3d2d77de2ecf62fb40c6d0025db6d2fdda3
Jani Arvonen <jani.arvonen@gmail.com>
2013-09-13 19:50:26 +0300

    should send DELETE request for unfollowing a user

--- test/whereabts/functional/users_follow_api_functest.clj
+++ test/whereabts/functional/users_follow_api_functest.clj
@@ -12,8 +12,12 @@
 (defn- whereabts-follow-user-api [user-id] 
 	(str whereabts-api-testsrv "/user/" user-id "/followers"))
 
-(defn post-follow-as [follower following]
+(defn- post-follow-as [follower following]
 	(http/post (whereabts-follow-user-api (.toString (:_id following)))
+		(whereabts-api-request [(:email follower) (:user-uuid follower)] "")))
+
+(defn- post-unfollow-as [follower following]
+	(http/delete (whereabts-follow-user-api (.toString (:_id following)))
 		(whereabts-api-request [(:email follower) (:user-uuid follower)] "")))
 
 (background (before :facts (setup-db)))
@@ -23,3 +27,6 @@
 
 (fact "should POST follow request for nonexisting user and return http not found"
 	(:status (post-follow-as test-user-b {:_id (ObjectId.)})) => 404)
+
+(fact "should send DELETE request for unfollowing a user"
+	(:status (post-unfollow-as test-user-b test-user-a)) => 200)
