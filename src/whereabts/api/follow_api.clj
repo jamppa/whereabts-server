bc9fd78f64b8463da54974d7a930c5c282966975
Jani Arvonen <jani.arvonen@gmail.com>
2013-09-13 13:38:59 +0300

    provided follow-user in follow-api-routes

--- src/whereabts/api/follow_api.clj
+++ src/whereabts/api/follow_api.clj
@@ -1,15 +1,18 @@
 (ns whereabts.api.follow-api
 	(:use 
 		whereabts.api.api-utils
+		whereabts.core.users-follow
 		compojure.core
 		ring.util.response
 		[clojure.walk :only [keywordize-keys]]))
 
 (defroutes follow-api-routes
 
-	(POST "/user/:id/followers" [:as req]
+	(POST "/user/:id/followers" [id :as req]
 		(with-role req ["email"]
-			(-> (response {})
+			(-> 
+				(follow-user id (:basic-authentication req))
+				(response)
 				(status 201))))
 
 	(DELETE "/user/:id/followers" [:as req]
--- test/whereabts/api/follow_api_test.clj
+++ test/whereabts/api/follow_api_test.clj
@@ -3,14 +3,17 @@
 		midje.sweet
 		ring.mock.request
 		whereabts.api.follow-api
-		whereabts.api-helper))
+		whereabts.api-helper
+		whereabts.core.users-follow))
 
 (def response-followed (expected-res 201 {}))
 (def response-unfollowed (expected-res 200 {}))
 
 (fact "should POST to user followers"
 	(follow-api-routes 
-		(whereabts-request-as-anonymous-user :post "/user/123AbC/followers" {})) => response-followed)
+		(whereabts-request-as-anonymous-user :post "/user/123AbC/followers" {})) => response-followed
+	(provided
+		(follow-user "123AbC" email-roled-user) => {} :times 1))
 
 (fact "should DELETE from user followers"
 	(follow-api-routes
