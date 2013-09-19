ff2a353981535b2453d5ae8797b0b6badb8fbf7e
Jani Arvonen <jani.arvonen@gmail.com>
2013-09-19 20:20:12 +0300

    introduced notification.follow-notification

---
+++ src/whereabts/notification/follow_notification.clj
@@ -0,0 +1,4 @@
+(ns whereabts.notification.follow-notification
+	(:use 
+		whereabts.notification.core
+		whereabts.models.util))
---
+++ test/whereabts/notification/follow_notification_test.clj
@@ -0,0 +1,6 @@
+(ns whereabts.notification.follow-notification-test
+	(:use 
+		midje.sweet
+		whereabts.notification.follow-notification
+		whereabts.notification.core
+		whereabts.db.user-test-fixtures))
