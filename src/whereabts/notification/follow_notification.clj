d964d900422c526481ff6393074642de3e44e484
Jani Arvonen <jani.arvonen@gmail.com>
2013-09-20 10:35:17 +0300

    should prepare follow message for channel

--- src/whereabts/notification/follow_notification.clj
+++ src/whereabts/notification/follow_notification.clj
@@ -2,3 +2,8 @@
 	(:use 
 		whereabts.notification.core
 		whereabts.models.util))
+
+(defn prepare-follow-message-for-channel [follower following]
+	{
+		:follower-id (obj-id-as-str follower)
+		:following-id (obj-id-as-str following)})
--- test/whereabts/notification/follow_notification_test.clj
+++ test/whereabts/notification/follow_notification_test.clj
@@ -4,3 +4,14 @@
 		whereabts.notification.follow-notification
 		whereabts.notification.core
 		whereabts.db.user-test-fixtures))
+
+(def follower test-user-a)
+(def following test-user-b)
+
+(def follow-channel-message {
+	:follower-id (.toString (:_id test-user-a))
+	:following-id (.toString (:_id test-user-b))
+	})
+
+(fact "should prepare message for channel"
+	(prepare-follow-message-for-channel follower following) => follow-channel-message)
